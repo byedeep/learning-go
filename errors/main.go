f0060fe3591ef62846d7a03629e71ab33e63af29
byedeep <byedeep1@gmail.com>
2024-12-15 02:18:18 +0530

    added code to practice errors

--- errors/main.go
+++ errors/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func divide(x, y float64) (float64, error) {
 	if y == 0 {
-		return 0, fmt.Errorf("Error!")
+		return 0, errors.New("Error!")
 	}
 	return x / y, nil
 }
